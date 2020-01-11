c31024b02c03db50a1e3617bd1ab262482d39b83
Vinshuka <melissa.ann.christie@gmail.com>
2020-01-10 23:20:07 -0500

    MVC1 added to repo

---
+++ MVC1/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewData["FromController"] = "This is from the controller";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Services()
+        {
+            ViewBag.Message = "Services page.";
+            ViewData["ServicesController"] = "We offer lots of great services.";
+
+            return View();
+        }
+
+    }
+}
